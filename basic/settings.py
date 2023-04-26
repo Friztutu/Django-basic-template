654364a1fb832928d17eaf83d3db011de92156ce
Aleksandr Erofeev <erofeevs943@gmail.com>
2023-04-26 03:43:56 -0700

    FIX Issue with static files

--- basic/settings.py
+++ basic/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from basic.secret_key import KEY
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,8 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (
-    BASE_DIR / 'static',
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
